6a8ec9f0650fdda8d64979907e56e9882ece9190
Hashini Wickrama Arachchi <85722523+HA-01@users.noreply.github.com>
2022-04-22 13:57:44 +0530

    CORS error correction

--- Anonymous_Tutor/flask_backend/main.py
+++ Anonymous_Tutor/flask_backend/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, Response
+from flask_cors import CORS, cross_origin
+
 import cv2
 
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 video = cv2.VideoCapture(0)
 
@@ -179,6 +183,7 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
                
 @app.route('/video_feed')
+@cross_origin(supports_credentials=True)
 def video_feed():
     return Response(gen(video),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -188,6 +193,7 @@ def test(camera):
     return text
            
 @app.route('/profile')
+@cross_origin(supports_credentials=True)
 def my_profile():
     text = test(video)
     response_body = {
